8f54637f624b736b810b431d80f24b3a134f80c2
Dylan Puffenbarger <dylanpuffenbarger@gmail.com>
2023-10-27 18:13:06 -0500

    fixed fetchPokemon() and defined parsePokemon()

--- src/index.js
+++ src/index.js
@@ -8,7 +8,15 @@ const output_table = {
   'prev_pkmn': document.getElementById('prev-pkmn'),
   'next_pkmn': document.getElementById('next-pkmn')
 }
+const totalPokemon = 1017;
 
+const titleCase = (str) => {
+  let words = str.split(' ');
+  if(words.length === 1){
+    return words[0][0].toUpperCase() + words[0].slice(1);
+  }
+  return ([titleCase(words[0])].concat(words.slice(1))).join(' ');
+}
 const copyValuesByKey = (obj_in, keys) => {
   let obj_out = {};
   
@@ -17,23 +25,59 @@ const copyValuesByKey = (obj_in, keys) => {
   return obj_out;
 }
 
-function fetchPokemon(input) {
-  const fetchSpeciesHelper = (json_in) => {
-    return {
-      'species': copyValuesByKey(json_in, ['names','id','flavor_text_entries']),
-      'pokemon': fetch(json_in.varieties[0].pokemon.url)
-        .then((resp) => resp.json())
-        .then((json) => fetchPokemonHelper(json))
-    };
-  }
-  const fetchPokemonHelper = (json_in) => {
-    return copyValuesByKey(
-      json_in,
-      ['sprites', 'types', 'abilities']
-    );
-  }
+function fetchPokemon(input, is_button = false) {
+  const fetchHelper = (spcs_json) => {
+    return fetch(spcs_json.varieties[0].pokemon.url)
+    .then((resp) => resp.json())
+    .then((pkmn_json) => parsePokemon(pkmn_json, spcs_json, is_button))
+  };
 
   return fetch(`https://pokeapi.co/api/v2/pokemon-species/${input}`)
     .then((resp) => resp.json())
-    .then((json) => fetchSpeciesHelper(json));
+    .then((json) => fetchHelper(json));
+}
+
+function parsePokemon(pokemon, species, is_button = false){
+  // console.log(pokemon,species);
+
+  const getSprite = () => pokemon.sprites.front_default;
+  const getName = () => {
+    return species.names.find((entry) => entry.language.name === 'en').name;
+  }
+  const getSmallSprite = () => {
+    return pokemon.sprites.versions['generation-viii'].icons.front_default
+  }
+  const getTypes = () => {
+    let types = [];
+    pokemon.types.forEach(
+      (entry) => types.push(entry.type.name)
+    );
+    return types;
+  }
+  const getFlavorText = () => {
+    return species.flavor_text_entries
+    .findLast((entry) => entry.language.name === 'en').flavor_text;
+  }
+  const getAbilities = () => {
+    let abilities = [];
+    pokemon.abilities.forEach(
+      (entry) => abilities.push(
+        {
+          'name':       titleCase(entry.ability.name.replace('-',' ')),
+          'is_hidden':  entry.is_hidden
+        }
+      ));
+    return abilities;
+  }
+
+  const data_out = {
+    'sprite':       getSprite(),
+    'name':         getName(),
+    'dex_no':       species.id,
+    'types':        getTypes(),
+    'flavor_text':  getFlavorText(),
+    'abilities':    getAbilities()
+  }
+  console.log(data_out)
+  // renderPkmn(data_out);
 }
